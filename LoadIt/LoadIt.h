ce6509bc59e2f570e353ae6c0f1e3e13bfa79a13
Luciano Marisi <luciano@techbrewers.com>
2016-06-25 23:03:17 +0100

    Initial commit

---
+++ LoadIt/LoadIt.h
@@ -0,0 +1,19 @@
+//
+//  LoadIt.h
+//  LoadIt
+//
+//  Created by Luciano Marisi on 25/06/2016.
+//  Copyright © 2016 Luciano Marisi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoadIt.
+FOUNDATION_EXPORT double LoadItVersionNumber;
+
+//! Project version string for LoadIt.
+FOUNDATION_EXPORT const unsigned char LoadItVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadIt/PublicHeader.h>
+
+
