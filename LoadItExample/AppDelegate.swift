69027ea7234af49a70646451667e911c2da381fb
Luciano Marisi <luciano@techbrewers.com>
2016-06-25 23:05:24 +0100

    Added empty sample app

---
+++ LoadItExample/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  LoadItExample
+//
+//  Created by Luciano Marisi on 25/06/2016.
+//  Copyright © 2016 Luciano Marisi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+}
+
---
+++ LoadItExample/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  LoadItExample
+//
+//  Created by Luciano Marisi on 25/06/2016.
+//  Copyright © 2016 Luciano Marisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
