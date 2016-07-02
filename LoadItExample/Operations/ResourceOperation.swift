7629688f2d69abf1b6a14de085d0116c8fea2af8
Luciano Marisi <luciano@techbrewers.com>
2016-07-02 19:47:42 +0100

    Added CitiesResourceOperation subclass of ResourceOperation example

--- LoadItExample/Operations/ResourceOperation.swift
+++ LoadItExample/Operations/ResourceOperation.swift
@@ -9,16 +9,16 @@
 import Foundation
 import LoadIt
 
-final class ResourceOperation<ResourceService: ResourceServiceType>: BaseOperation, ResourceOperationType {
+class ResourceOperation<ResourceService: ResourceServiceType>: BaseOperation, ResourceOperationType {
   
   typealias Resource = ResourceService.Resource
   typealias DidFinishFetchingResourceCallback = (ResourceOperation<ResourceService>, Result<Resource.Model>) -> Void
 
   private let resource: Resource
   private let service: ResourceService  
-  private let didFinishFetchingResourceCallback: DidFinishFetchingResourceCallback
+  private let didFinishFetchingResourceCallback: DidFinishFetchingResourceCallback?
   
-  init(resource: ResourceService.Resource, service: ResourceService = ResourceService(), didFinishFetchingResourceCallback: DidFinishFetchingResourceCallback) {
+  init(resource: ResourceService.Resource, service: ResourceService = ResourceService(), didFinishFetchingResourceCallback: DidFinishFetchingResourceCallback? = nil) {
     self.resource = resource
     self.service = service
     self.didFinishFetchingResourceCallback = didFinishFetchingResourceCallback
@@ -30,7 +30,26 @@ final class ResourceOperation<ResourceService: ResourceServiceType>: BaseOperati
   }
   
   func didFinishFetchingResource(result result: Result<Resource.Model>) {
-    didFinishFetchingResourceCallback(self, result)
+    didFinishFetchingResourceCallback?(self, result)
+  }
+  
+}
+
+protocol CitiesResourceOperationDelegate: class {
+  func citiesOperationDidFinish(operation: CitiesResourceOperation, result: Result<[City]>)
+}
+
+final class CitiesResourceOperation: ResourceOperation<DiskJSONService<CitiesResource>> {
+  
+  private weak var delegate: CitiesResourceOperationDelegate?
+  
+  init(resource: CitiesResource, delegate: CitiesResourceOperationDelegate) {
+    self.delegate = delegate
+    super.init(resource: resource)
+  }
+  
+  override func didFinishFetchingResource(result result: Result<[City]>) {
+    self.delegate?.citiesOperationDidFinish(self, result: result)
   }
   
 }
