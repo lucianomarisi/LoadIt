98a27851bf17aea5b3fdb3862a8e05b589ee2915
Luciano Marisi <luciano@techbrewers.com>
2016-07-20 22:25:14 +0100

    Added SubclassedNetworkJSONResourceService tests

---
+++ LoadItTests/SubclassedNetworkJSONResourceServiceTests.swift
@@ -0,0 +1,62 @@
+//
+//  SubclassedNetworkJSONResourceServiceTests.swift
+//  LoadIt
+//
+//  Created by Luciano Marisi on 20/07/2016.
+//  Copyright © 2016 Luciano Marisi. All rights reserved.
+//
+
+import XCTest
+@testable import LoadIt
+
+private let commonKey = "common"
+
+final class SubclassedNetworkJSONResourceService<Resource: NetworkJSONResourceType>: NetworkJSONResourceService<Resource> {
+  
+  // Needed because of https://bugs.swift.org/browse/SR-416
+  required init() {
+    super.init()
+  }
+  
+  override init(session: URLSessionType) {
+    super.init(session: session)
+  }
+  
+  override func additionalHeaderFields() -> [String: String] {
+    return [commonKey: "subclass"]
+  }
+}
+
+private let testURL = NSURL(string: "http://test.com")!
+private let url = NSURL(string: "www.test.com")!
+
+class SubclassedNetworkJSONResourceServiceTests: XCTestCase {
+  
+  var testService: SubclassedNetworkJSONResourceService<MockNetworkJSONResource>!
+  var mockSession: MockURLSession!
+  var mockResource: MockNetworkJSONResource!
+  
+  let testRequest = NSURLRequest(URL: testURL)
+  
+  override func setUp() {
+    super.setUp()
+    mockSession = MockURLSession()
+    testService = SubclassedNetworkJSONResourceService<MockNetworkJSONResource>(session: mockSession)
+  }
+  
+  func test_subclassHTTPHeaderFields_areOverridenByResourceHTTPHeaderFields() {
+    let resourceHTTPHeaderFields = [commonKey: "resource"]
+    mockResource = MockNetworkJSONResource(url: testURL, HTTPHeaderFields: resourceHTTPHeaderFields)
+    testService.fetch(resource: mockResource) { _ in }
+    XCTAssertEqual(mockSession.capturedRequest!.allHTTPHeaderFields!, resourceHTTPHeaderFields)
+  }
+  
+  func test_finalRequestInclude_subclassHTTPHeaderFields_and_resourceHTTPHeaderFields() {
+    let resourceHTTPHeaderFields = ["resource_key" : "resource"]
+    mockResource = MockNetworkJSONResource(url: testURL, HTTPHeaderFields: resourceHTTPHeaderFields)
+    testService.fetch(resource: mockResource) { _ in }
+    let expectedHeaderFields = [commonKey: "subclass", "resource_key" : "resource"]
+    XCTAssertEqual(mockSession.capturedRequest!.allHTTPHeaderFields!, expectedHeaderFields)
+  }
+  
+}
